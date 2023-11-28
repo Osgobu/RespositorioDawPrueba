2acd8d00b8e5f35b1740872cc8fe4beffd741f41
«Oscar» <oscargbuisa@gmail.com>
2023-11-28 13:23:52 +0100

    He camiado de 20 a 33

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range (0, 33):
-    print("Esatre atento a las explicaciones para preguntar lo mismo 20 veces")
+    print("Esatre atento a las explicaciones para preguntar lo mismo 33 veces")
